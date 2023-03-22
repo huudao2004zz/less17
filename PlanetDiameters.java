83c8db884e3fd490961bc6c83d10d8b0767260d0
huudao2004zz <111514495+huudao2004zz@users.noreply.github.com>
2023-03-22 15:00:07 +0700

    Add files via upload

--- PlanetDiameters.java
+++ PlanetDiameters.java
@@ -4,12 +4,11 @@ import java.util.*;
 public class PlanetDiameters {
 
     private static final String[] names = {"Mercury","Venus","Earth","Mars","Jupiter","Saturn","Uranus","Neptune","Pluto" };
-
+    private static final float[] diameters = { 4800f, 12103.6f, 12756.3f, 6794f, 142984f, 120536f, 51118f, 49532f, 2274f };
     public static void main(String[] args) {
         Map planets = new HashMap();
 
-        Object[] diameters = new Object[0];
-        for (int i = 0; i<names.length; i++)
+        for (int i = 0 ; i<names.length;i++)
             planets.put(names[i], diameters[i]);
 
         Iterator it = planets.entrySet().iterator();
