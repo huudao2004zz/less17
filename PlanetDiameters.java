d7d6b3894eb6c3f31346a9a0b16054e561087f9b
huudao2004zz <111514495+huudao2004zz@users.noreply.github.com>
2023-03-22 15:19:25 +0700

    Add files via upload

--- PlanetDiameters.java
+++ PlanetDiameters.java
@@ -1,19 +1,22 @@
 import java.util.*;
 
-
 public class PlanetDiameters {
 
     private static final String[] names = {"Mercury","Venus","Earth","Mars","Jupiter","Saturn","Uranus","Neptune","Pluto" };
     private static final float[] diameters = { 4800f, 12103.6f, 12756.3f, 6794f, 142984f, 120536f, 51118f, 49532f, 2274f };
     public static void main(String[] args) {
-        Map planets = new HashMap();
+        // Using LinkedHashMap
+        //Map<String, Float> planets = new LinkedHashMap<>();
+
+        // Using TreeMap
+        Map<String, Float> planets = new TreeMap<>();
 
         for (int i = 0 ; i<names.length;i++)
             planets.put(names[i], diameters[i]);
 
-        Iterator it = planets.entrySet().iterator();
+        Iterator<Map.Entry<String, Float>> it = planets.entrySet().iterator();
         while (it.hasNext()){
-            Map.Entry e = (Map.Entry) it.next();
+            Map.Entry<String, Float> e = it.next();
             System.out.printf(e.getKey() + ": " + e.getValue());
         }
         System.out.printf("\n------------------------\n");
@@ -22,7 +25,7 @@ public class PlanetDiameters {
             String name = (new Scanner(System.in)).nextLine();
             if (name.length() == 0) break;
             if (!planets.containsKey(name))
-            System.out.printf("Planet "+ name + "not found! ");
+                System.out.printf("Planet "+ name + "not found! ");
             else
                 System.out.printf("The diameter of "+ name + "is" + planets.get(name));
         }
